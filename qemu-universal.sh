856ebae4de4bf6e55c1bb8987190c48a9b22b175
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-03-31 21:58:58 -0300

    Add fakeroot to installation and fix chroot command

--- qemu-universal.sh
+++ qemu-universal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install desktop-file-utils debootstrap schroot perl git wget xz-utils bubblewrap autoconf coreutils
+sudo apt-get install desktop-file-utils debootstrap schroot perl git wget xz-utils bubblewrap autoconf coreutils fakeroot -y
 wget -q "https://github.com/VHSgunzo/uruntime/releases/download/v0.5.7/uruntime-appimage-dwarfs-x86_64" -O uruntime && chmod a+x uruntime
 wget -c "https://archive.archlinux.org/iso/"
 cat index.html | tail -n 3 | awk '{print $2}' | cut -d "/" -f 1 | cut -d "\"" -f 2 | xargs -i -t -exec wget -r --no-parent -np -l 1 -A "*.zst" -erobots=off -P . "https://archive.archlinux.org/iso/{}/archlinux-bootstrap-x86_64.tar.zst"
@@ -9,7 +9,7 @@ tar xf archlinux-bootstrap-x86_64.tar.zst -C ./arch/
 # criar no github uma nova pasta parao AppRun e demais arquivos.
 cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/ && cp ${GITHUB_WORKSPACE}/files/mirrorlist -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/pacman.d/ && cp ${GITHUB_WORKSPACE}/files/pacman.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64/etc/
 cd ${GITHUB_WORKSPACE}
-sudo chroot ./arch/root.x86_64/ /bin/bash -c "pacman -Syyu --noconfirm && pacman -S qemu-full jack2 --noconfirm && rm -rf /var/cache/pacman/pkg/* && exit"
+fake -- chroot ./arch/root.x86_64/ /bin/bash -c "pacman -Syyu --noconfirm && pacman -S qemu-full jack2 --noconfirm && rm -rf /var/cache/pacman/pkg/* && exit"
 cp ${GITHUB_WORKSPACE}/files/AppRun ${GITHUB_WORKSPACE}/arch/ && chmod a+x ${GITHUB_WORKSPACE}/arch/AppRun && cp ${GITHUB_WORKSPACE}/files/qemu.svg -t ${GITHUB_WORKSPACE}/arch/ && cp ${GITHUB_WORKSPACE}/files/qemu.desktop -t ${GITHUB_WORKSPACE}/arch/
 mv ${GITHUB_WORKSPACE}/arch/root.x86_64/ ${GITHUB_WORKSPACE}/arch/root/
 find ${GITHUB_WORKSPACE}/arch/root/usr/bin/ -type f -exec strip {} +
